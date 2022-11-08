07df1bb2ddb095ce6f09dd15b5c02c9c34cc97da
Anbarasan Vellaiyan <anbarasan.vellaiyan@cytel.com>
2022-11-08 19:09:15 +0530

    added-payment-delay-feature

--- payment-micro-service/payment_micro_service/main.py
+++ payment-micro-service/payment_micro_service/main.py
@@ -1,10 +1,10 @@
-import time
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
 import requests
+import time
 
 
 app = FastAPI()
@@ -39,14 +39,25 @@ class Order(HashModel):
         database = redis
 
 
+@app.get('/orders')
+def get_order():
+    return [Order.get(pk) for pk in Order.all_pks()]
+
+
+@app.get('/orders/{order_key}')
+def get_order(order_key: str):
+    return Order.get(order_key)
+
+
 @app.post("/orders")
-async def create_orders(request: Request):
+async def create_orders(request: Request, background_tasks: BackgroundTasks):
     body = await request.json()
     req = requests.get(f'http://localhost:8000/products/{body.get("id")}')
     product = req.json()
 
     order = Order(
         product_id=body['id'],
+        name=product['name'],
         price=product['price'],
         fee=0.2 * product['price'],
         total=1.2 * product['price'],
@@ -54,7 +65,7 @@ async def create_orders(request: Request):
         status='pending'
     )
     order.save()
-    order_completed(order)
+    background_tasks.add_task(order_completed, order)
     return order
 
 
@@ -62,3 +73,8 @@ def order_completed(order: Order):
     time.sleep(5)
     order.status = 'completed'
     order.save()
+
+
+@app.delete("/orders/{order_key}")
+def delete_order(order_key: str):
+    return Order.delete(order_key)
