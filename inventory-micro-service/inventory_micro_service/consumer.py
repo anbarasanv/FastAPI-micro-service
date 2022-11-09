4d55b1d26329498eca3be95696326eb883128197
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2022-11-09 10:55:33 +0530

    added-payment-streams

---
+++ inventory-micro-service/inventory_micro_service/consumer.py
@@ -0,0 +1,29 @@
+from main import redis, Product
+import time
+
+
+key = 'order_completed'
+group = 'inventory-group'
+
+
+try:
+    redis.xgroup_create(key, group)
+except Exception as e:
+    print('Group Already exists')
+
+while True:
+    try:
+        results = redis.xreadgroup(group, key, {key: '>'}, None)
+        if results:
+            for result in results:
+                obj = result[1][0][1]
+                try:
+                    product = Product.get(obj.get('product_id'))
+                    product.quantity = product.quantity - int(obj.get('quantity'))
+                    product.save()
+                except Exception as e:
+                    redis.xadd('refund_order', obj, '*')
+    except Exception as e:
+        print(str(e))
+    time.sleep(1)
+
